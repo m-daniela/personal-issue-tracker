f71d13bb8be944c12b1b49a7ec2623e180881302
m-daniela <54980615+m-daniela@users.noreply.github.com>
2023-08-28 11:23:25 +0300

    FE added common components and pages skeleton

---
+++ src/app/projects/[projectId]/category/[categoryId]/task/[taskId]/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TaskPage = () => {
+    return (
+        <section>
+            task page
+        </section>
+    );
+};
+
+export default TaskPage;
---
+++ src/utils/generalConstants.js
@@ -0,0 +1,28 @@
+
+const mainUrlApi = process.env.NEXT_PUBLIC_API_URL;
+
+export const apiUrls = {
+    getProjects: `${mainUrlApi}projects`,
+    createProject: `${mainUrlApi}projects`,
+    updateProject: (projectId) => `${mainUrlApi}projects/${projectId}`,
+    deleteProject: (projectId) => `${mainUrlApi}projects/${projectId}`,
+    getCategoriesAndTasks: (projectId) => `${mainUrlApi}projects/${projectId}/tasks`,
+    getTask: (projectId, categoryId, taskId) => `${mainUrlApi}projects/${projectId}
+    /category/${categoryId}/task/${taskId}`,
+    addTask: (projectId, categoryId) => `${mainUrlApi}projects/${projectId}
+    /category/${categoryId}/task`,
+    updateTask: (projectId, categoryId, taskId) => `${mainUrlApi}projects/${projectId}
+    /category/${categoryId}/task/${taskId}`,
+    deleteTask: (projectId, categoryId, taskId) => `${mainUrlApi}projects/${projectId}
+    /category/${categoryId}/task/${taskId}`,
+
+};
+
+export const routes = {
+    mainPage: "/",
+    projectsRoute: "/projects",
+    projectRoute: (projectId) => `/projects/${projectId}`,
+    taskRoute: (projectId, categoryId, taskId) => `/projects/${projectId}
+    /category/${categoryId}/task/${taskId}`,
+    addTaskRoute: (projectId, categoryId) => `/projects/${projectId}/category/${categoryId}/task`,
+};
